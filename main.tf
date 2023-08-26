838a0b53a5abf629cb52339e66ef734de27befa5
zmwei <zhiminwei@gmail.com>
2023-08-26 12:22:55 +0800

    fix bucket name

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "instances"  {  #this is just gonna be a name for terraf
 
 terraform {
   backend "s3" {
-    bucket = "sctp-ce3-tfstate-bkt"
+    bucket = "sctp-ce3-tfstate-bucket"
     key = "zmwei.tfstate"
     region = "us-east-1"
   }
